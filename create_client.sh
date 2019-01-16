a267b06183f6cbe4d99faad23378de640c660a56
thedcspace <36161204+thedcspace@users.noreply.github.com>
2019-01-16 11:00:42 +0000

    Update create_client.sh

--- create_client.sh
+++ create_client.sh
@@ -1,1 +1,9 @@
 #THis will be the script used to create the client certificates for OpenVPN
+#!/bin/bash
+
+read -p "Enter Client Name: " clientName
+
+. /etc/openvpn/easy-rsa/vars
+./build-key $clientName
+
+
